acf8f55a70ff4116c320fa5427811e185476b39f
jinseo1200 <87170034+jinseo1200@users.noreply.github.com>
2021-08-24 16:08:55 +0900

    20210824

---
+++ if형성평가1.py
@@ -0,0 +1,10 @@
+num1 = int(input("수를 입력하세요"))
+
+num2 = int(input("수를 입력하세요"))
+
+if num1 > num2:
+    print(num1-num2)
+else :
+    print(num2-num1)
+#삼항연산자 사용
+# result = num1-num2 if num1 > num2 else num2-num1
