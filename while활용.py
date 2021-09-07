3d49a618082098a0bb3236c0e3c145d9b48dce1b
jinseo1200 <87170034+jinseo1200@users.noreply.github.com>
2021-09-07 16:39:28 +0900

    20210907

--- while활용.py
+++ while활용.py
@@ -1,4 +1,22 @@
+"""
 num = 0
-while(num < 30):
+sum_data = 0
+while(num < 10):
     num = num + 1
     print(num)
+    sum_data = sum_data + num
+    print("누적합:%d"%sum_data)
+"""
+num = int(input("100이하에 정수를 입력하세요"))
+if num > 100 or num < 1:
+    print("붹")
+    exit()
+
+num1 = 0
+sum_data = 0
+
+while(num1 < num):
+    num1 = num1 + 1
+    print(num1)
+    sum_data = sum_data + num1
+    print("누적합:%d" %sum_data)
